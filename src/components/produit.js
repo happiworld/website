48e71ae7973038f4b9122c6d3f393e185fb72495
Steven Enten <steven@enten.fr>
2018-06-13 23:53:30 +0200

    Frontend: add cors headers (2)

--- src/components/produit.js
+++ src/components/produit.js
@@ -43,6 +43,7 @@ class Produit extends React.Component {
     axios.post(PAYMENT_URL, { produit, token }, {
       headers: {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
       }
     })
       .then(data => {
